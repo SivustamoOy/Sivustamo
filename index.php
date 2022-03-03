a3cc61eb3dbfd104966009322b5a6ffb8f63da39
Matti Mieskonen <matti@uuru.fi>
2022-03-03 08:40:41 +0200

    Aloitus

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:       Sivustamo base
+ * Plugin URI:        https://github.com/SivustamoOy/Sivustamo
+ * Description:       Peruskoodit jokaiselle sivustolle
+ * Version:           1.0.0
+ * Author:            Matti Mieskonen
+ * License:           Closed
+ * GitHub Plugin URI: https://github.com/SivustamoOy/Sivustamo
+ * GitHub Plugin URI: SivustamoOy/Sivustamo
+ * Primary Branch:    main
+ */
