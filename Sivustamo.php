7abb99049b33de86429fa4f3b9790e97747f15d3
Matti Mieskonen <matti@uuru.fi>
2022-05-30 14:40:19 +0300

    Versio 2.1.0 Admin dashboard

--- Sivustamo.php
+++ Sivustamo.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Plugin Name:       Sivustamo
  * Plugin URI:        https://github.com/SivustamoOy/Sivustamo
  * Description:       Peruskoodit jokaiselle sivustolle
- * Version:           1.0.4
+ * Version:           2.1.0
  * Author:            Matti Mieskonen
  * License:           Closed
  * GitHub Plugin URI: https://github.com/SivustamoOy/Sivustamo
@@ -11,8 +12,123 @@
  * Primary Branch:    main
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+class Sivustamo_Activator
+{
+    public static function activate()
+    {
+        add_option('redirect_404s', 0);
+        add_option('wpb_stop_update_emails', 0);
+        add_option('backtopwp', 0);
+        add_option('sivustamo_tukiviesti', 0);
+    }
+}
+
+class Sivustamo_Deactivator
+{
+    public static function deactivate()
+    {
+        delete_option('redirect_404s');
+        delete_option('wpb_stop_update_emails');
+        delete_option('backtopwp');
+        delete_option('sivustamo_tukiviesti');
+    }
+
+}
+
+function activate_sivustamo()
+{
+    Sivustamo_Activator::activate();
+}
+
+function deactivate_sivustamo()
+{
+    Sivustamo_Deactivator::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_sivustamo');
+register_deactivation_hook(__FILE__, 'deactivate_sivustamo');
+
+class Sivustamo
+{
+    function __construct() {
+        self::loadSettings();
+    }
+    public function loadSettings() {
+        add_action('admin_menu', array($this, 'sivustamo_plugin_setup_menu'));
+    }
+
+    function sivustamo_plugin_setup_menu()
+    {
+        add_menu_page('Sivustamo', 'Sivustamo', 'manage_options', 'sivustamo-plugin', array($this, 'sivustamo_init'));
+    }
+
+    function sivustamo_init()
+    {
+        if (isset($_GET['sivustamo-plugin-save'])){
+            // TODO --
+            // each of these update_option functions should sanitize data first (not shown in this example)
+            if (isset($_POST['redirect_404s'])){
+                update_option('redirect_404s', $_POST['redirect_404s']);
+            } else {
+                update_option('redirect_404s', 0);
+            }
+            if (isset($_POST['wpb_stop_update_emails'])){
+                update_option('wpb_stop_update_emails', $_POST['wpb_stop_update_emails']);
+            } else {
+                update_option('wpb_stop_update_emails', 0);
+            }
+            if (isset($_POST['backtopwp'])){
+                update_option('backtopwp', $_POST['backtopwp']);
+            } else {
+                update_option('backtopwp', 0);
+            }
+            if (isset($_POST['sivustamo_tukiviesti'])){
+                update_option('sivustamo_tukiviesti', $_POST['sivustamo_tukiviesti']);
+            } else {
+                update_option('sivustamo_tukiviesti', 0);
+            }
+            // redirect back to form
+            wp_redirect($_SERVER['HTTP_REFERER']);
+            exit();
+        }
+        echo "<h1>Sivustamo - Asetukset!</h1>";
+        $my_theme = wp_get_theme();
+        ?>
+        <form action="<?php print $_SERVER['PHP_SELF']; ?>?page=sivustamo-plugin&noheader=true&sivustamo-plugin-save=true"
+              method="post">
+            <div>
+                <label>Redirect 404: </label><input type="checkbox" name="redirect_404s" value="1" <?php if (get_option('redirect_404s')==1) echo 'checked="checked"'; ?>/>
+                <p class="description">Ohjaa 404 virheet etusivulle.</p>
+            </div>
+            <div>
+                <label>Stop autoupdate success mails: </label><input type="checkbox" name="wpb_stop_update_emails" value="1" <?php if (get_option('wpb_stop_update_emails')==1) echo 'checked="checked"'; ?>/>
+                <p class="description">Estä päivitysten onnistumisen sähköpostit.</p>
+            </div>
+            <div>
+                <label>Hide back to wodpress -button: </label><input type="checkbox" name="backtopwp" value="1" <?php if (get_option('backtopwp')==1) echo 'checked="checked"'; ?>/>
+                <p class="description">Nappi 'takaisin wordpressiin' piiloon.</p>
+            </div>
+            <div>
+                <label>Sivustamo support widget: </label><input type="checkbox" name="sivustamo_tukiviesti" value="1" <?php if (get_option('sivustamo_tukiviesti')==1) echo 'checked="checked"'; ?>/>
+                <p class="description">Admin yhteystiedot.</p>
+            </div>
+
+            <?php submit_button(); ?>
+        </form>
+            <?php
+
+    }
+}
+$si=new Sivustamo();
+/* admin init */
+
 /* 404 ohjaus etusivulle */
-if (function_exists('redirect_404s')) {
+if ( (!function_exists('redirect_404s') ) && (get_option('redirect_404s')==1) ) {
     function redirect_404s()
     {
         if (is_404()) {
@@ -24,7 +140,7 @@ if (function_exists('redirect_404s')) {
 }
 
 /* update sähköpostien esto */
-if (function_exists('wpb_stop_update_emails')) {
+if ( (!function_exists('wpb_stop_update_emails') ) && (get_option('wpb_stop_update_emails')==1) ){
     function wpb_stop_update_emails($send, $type, $core_update, $result)
     {
         if (!empty($type) && $type == 'success') {
@@ -38,7 +154,7 @@ if (function_exists('wpb_stop_update_emails')) {
     add_filter('auto_theme_update_send_email', '__return_false');
 }
 /* back to wordpress näppäin pois */
-if (function_exists('admin_footer')) {
+if  (get_option('backtopwp')==1) {
     add_action('admin_footer', function () {
 
         ?>
@@ -82,17 +198,17 @@ if (function_exists('admin_footer')) {
 }
 
 /* ylläpitoteksti asiakkaalle */
-if (function_exists('my_custom_dashboard_widgets')) {
-    add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+if ( (!function_exists('sivustamo_tukiviesti') ) && (get_option('sivustamo_tukiviesti')==1) ){
+    add_action('wp_dashboard_setup', 'sivustamo_tukiviesti');
 
-    function my_custom_dashboard_widgets()
+    function sivustamo_tukiviesti()
     {
         global $wp_meta_boxes;
 
-        wp_add_dashboard_widget('custom_help_widget', 'Sivustamon asiakaspalvelu', 'custom_dashboard_help');
+        wp_add_dashboard_widget('sivustamo_help_widget', 'Sivustamon asiakaspalvelu', 'sivustamo_dashboard_help');
     }
 
-    function custom_dashboard_help()
+    function sivustamo_dashboard_help()
     {
         echo '
 	<style>
@@ -123,7 +239,7 @@ hr.viiva1 {
 <p>Mikäli epäilet, ettei sivusto toimi kuten pitäisi, ota yhteyttä Sivustamon tukeen ja autamme pulmasi kanssa!</p>
 <a href="https://www.sivustamo.fi/tuki/" target="_blank" class="buttonsivustamo">Tukilomake</a>
 <a href="mailto:tuki@sivustamo.fi" class="buttonsivustamo">tuki@sivustamo.fi</a>
-<a href="tel:0401876687" class="buttonsivustamo">040 187 6687</a>
+<a href="tel:+358401876687" class="buttonsivustamo">040 187 6687</a>
 <br>
 <br>
 <hr class="viiva1">
