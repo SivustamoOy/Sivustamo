93dfd168a45568606375ef191eda00dcb43eb549
OdYnXien <96468525+OdYnXien@users.noreply.github.com>
2024-09-10 16:03:29 +0300

    Update Sivustamo.php

--- Sivustamo.php
+++ Sivustamo.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * Plugin Name:       Sivustamo
  * Plugin URI:        https://github.com/SivustamoOy/Sivustamo
  * Description:       Peruskoodit jokaiselle sivustolle
- * Version:           2.3.0
- * Author:            Esko Junnila
+ * Version:           2.6.2
+ * Author:            Esko Junnila / Sivustamo Oy
  * License:           Closed
  * GitHub Plugin URI: https://github.com/SivustamoOy/Sivustamo
  * Primary Branch:    main
@@ -15,61 +14,59 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
     die;
 }
-
-class Sivustamo_Activator
-{
-    public static function activate()
-    {
-        add_option('redirect_404s', 0);
-        add_option('wpb_stop_update_emails', 0);
-        add_option('backtopwp', 0);
-        add_option('sivustamo_tukiviesti', 0);
-    }
-}
-
-class Sivustamo_Deactivator
-{
-    public static function deactivate()
-    {
-        delete_option('redirect_404s');
-        delete_option('wpb_stop_update_emails');
-        delete_option('backtopwp');
-        delete_option('sivustamo_tukiviesti');
-    }
-}
-
-function activate_sivustamo()
-{
-    Sivustamo_Activator::activate();
-}
-
-function deactivate_sivustamo()
-{
-    Sivustamo_Deactivator::deactivate();
-}
-
-register_activation_hook(__FILE__, 'activate_sivustamo');
-register_deactivation_hook(__FILE__, 'deactivate_sivustamo');
 
 class Sivustamo
 {
     private $allowed_ips = [
-        '95.217.63.43',
         '95.216.12.251',
         '65.109.105.54',
         '135.181.118.89'
     ];
 
-    function __construct() {
-        self::loadSettings();
+    private $slack_email = 'yllapito-aaaadtfdvqqkdtjg5x4yte7zfq@sivustamo.slack.com';
+
+    function __construct()
+    {
+        $this->loadSettings();
         add_shortcode('sivustamo_link', array($this, 'sivustamo_link_shortcode'));
-        add_action('init', array($this, 'check_server_ip'));
+        add_action('admin_init', array($this, 'add_custom_header'));
+        
+        // WP-Cron tapahtuma
+        add_action('sivustamo_daily_ip_check', array($this, 'check_server_ip'));
     }
 
-    public function loadSettings() {
+    public function activate()
+    {
+        add_option('redirect_404s', 0);
+        add_option('wpb_stop_update_emails', 0);
+        add_option('backtopwp', 0);
+        add_option('sivustamo_tukiviesti', 0);
+        add_option('sivustamo_last_checked_ip', '');
+
+        if (!wp_next_scheduled('sivustamo_daily_ip_check')) {
+            wp_schedule_event(time(), 'daily', 'sivustamo_daily_ip_check');
+        }
+    }
+
+    public function deactivate()
+    {
+        delete_option('redirect_404s');
+        delete_option('wpb_stop_update_emails');
+        delete_option('backtopwp');
+        delete_option('sivustamo_tukiviesti');
+        delete_option('sivustamo_last_checked_ip');
+
+        $timestamp = wp_next_scheduled('sivustamo_daily_ip_check');
+        if ($timestamp) {
+            wp_unschedule_event($timestamp, 'sivustamo_daily_ip_check');
+        }
+    }
+
+    public function loadSettings()
+    {
         add_action('admin_menu', array($this, 'sivustamo_plugin_setup_menu'));
     }
 
@@ -80,23 +77,23 @@ class Sivustamo
 
     function sivustamo_init()
     {
-        if (isset($_GET['sivustamo-plugin-save'])){
-            if (isset($_POST['redirect_404s'])){
+        if (isset($_GET['sivustamo-plugin-save'])) {
+            if (isset($_POST['redirect_404s'])) {
                 update_option('redirect_404s', sanitize_text_field($_POST['redirect_404s']));
             } else {
                 update_option('redirect_404s', 0);
             }
-            if (isset($_POST['wpb_stop_update_emails'])){
+            if (isset($_POST['wpb_stop_update_emails'])) {
                 update_option('wpb_stop_update_emails', sanitize_text_field($_POST['wpb_stop_update_emails']));
             } else {
                 update_option('wpb_stop_update_emails', 0);
             }
-            if (isset($_POST['backtopwp'])){
+            if (isset($_POST['backtopwp'])) {
                 update_option('backtopwp', sanitize_text_field($_POST['backtopwp']));
             } else {
                 update_option('backtopwp', 0);
             }
-            if (isset($_POST['sivustamo_tukiviesti'])){
+            if (isset($_POST['sivustamo_tukiviesti'])) {
                 update_option('sivustamo_tukiviesti', sanitize_text_field($_POST['sivustamo_tukiviesti']));
             } else {
                 update_option('sivustamo_tukiviesti', 0);
@@ -110,19 +107,23 @@ class Sivustamo
         <form action="<?php print $_SERVER['PHP_SELF']; ?>?page=sivustamo-plugin&noheader=true&sivustamo-plugin-save=true"
               method="post">
             <div>
-                <label>Redirect 404: </label><input type="checkbox" name="redirect_404s" value="1" <?php if (get_option('redirect_404s')==1) echo 'checked="checked"'; ?>/>
+                <label>Redirect 404: </label><input type="checkbox" name="redirect_404s"
+                                                    value="1" <?php if (get_option('redirect_404s') == 1) echo 'checked="checked"'; ?>/>
                 <p class="description">Ohjaa 404 virheet etusivulle.</p>
             </div>
             <div>
-                <label>Stop autoupdate success mails: </label><input type="checkbox" name="wpb_stop_update_emails" value="1" <?php if (get_option('wpb_stop_update_emails')==1) echo 'checked="checked"'; ?>/>
+                <label>Stop autoupdate success mails: </label><input type="checkbox" name="wpb_stop_update_emails"
+                                                                     value="1" <?php if (get_option('wpb_stop_update_emails') == 1) echo 'checked="checked"'; ?>/>
                 <p class="description">Estä päivitysten onnistumisen sähköpostit.</p>
             </div>
             <div>
-                <label>Hide back to wodpress -button: </label><input type="checkbox" name="backtopwp" value="1" <?php if (get_option('backtopwp')==1) echo 'checked="checked"'; ?>/>
+                <label>Hide back to WordPress -button: </label><input type="checkbox" name="backtopwp"
+                                                                     value="1" <?php if (get_option('backtopwp') == 1) echo 'checked="checked"'; ?>/>
                 <p class="description">Nappi 'takaisin wordpressiin' piiloon.</p>
             </div>
             <div>
-                <label>Sivustamo support widget: </label><input type="checkbox" name="sivustamo_tukiviesti" value="1" <?php if (get_option('sivustamo_tukiviesti')==1) echo 'checked="checked"'; ?>/>
+                <label>Sivustamo support widget: </label><input type="checkbox" name="sivustamo_tukiviesti"
+                                                                value="1" <?php if (get_option('sivustamo_tukiviesti') == 1) echo 'checked="checked"'; ?>/>
                 <p class="description">Admin yhteystiedot.</p>
             </div>
 
@@ -131,53 +132,124 @@ class Sivustamo
         <?php
     }
 
-    public function sivustamo_link_shortcode() {
+    public function sivustamo_link_shortcode()
+    {
         if ($this->is_allowed_ip()) {
             return 'Kotisivut: <a title="Laitetaan yrityksesi näkyville!" href="https://www.sivustamo.fi" target="_blank" rel="noopener">Sivustamo Oy</a>';
         }
         return '';
     }
 
-    private function is_allowed_ip() {
+    private function is_allowed_ip()
+    {
         $server_ip = $_SERVER['SERVER_ADDR'];
         return in_array($server_ip, $this->allowed_ips);
     }
 
-    public function check_server_ip() {
-        if (!$this->is_allowed_ip()) {
-            $this->cleanup_plugins_and_settings();
+    public function check_server_ip()
+    {
+        $current_ip = $_SERVER['SERVER_ADDR'];
+        $last_checked_ip = get_option('sivustamo_last_checked_ip', '');
+
+        if ($current_ip !== $last_checked_ip) {
+            update_option('sivustamo_last_checked_ip', $current_ip);
+
+            if (!$this->is_allowed_ip()) {
+                $this->delayed_cleanup();
+                $this->send_notification();
+            }
         }
     }
 
-    private function cleanup_plugins_and_settings() {
-        // Deactivate and delete Perfmatters plugin if it exists
-        if (is_plugin_active('perfmatters/perfmatters.php')) {
-            deactivate_plugins('perfmatters/perfmatters.php');
-            delete_plugins(['perfmatters/perfmatters.php']);
+    private function send_notification()
+    {
+        $site_url = get_site_url();
+        $site_name = get_bloginfo('name');
+        $server_ip = $_SERVER['SERVER_ADDR'];
+
+        $subject = "Sivusto siirtynyt: {$site_name}";
+        $message = "Sivusto {$site_name} ({$site_url}) on havaittu uudessa IP-osoitteessa: {$server_ip}";
+
+        wp_mail($this->slack_email, $subject, $message);
+    }
+
+    public function delayed_cleanup()
+    {
+        try {
+            if ($this->can_delete_plugins()) {
+                $this->cleanup_plugins_and_settings();
+            } else {
+                update_option('sivustamo_needs_cleanup', true);
+                error_log('Sivustamo: Siivous merkitty suoritettavaksi myöhemmin.');
+            }
+        } catch (Exception $e) {
+            error_log('Sivustamo virhe: ' . $e->getMessage());
+        }
+    }
+
+    private function can_delete_plugins()
+    {
+        return current_user_can('delete_plugins') && wp_is_file_mod_allowed('delete_plugins');
+    }
+
+    private function cleanup_plugins_and_settings()
+    {
+        if (!function_exists('delete_plugins')) {
+            require_once(ABSPATH . 'wp-admin/includes/plugin.php');
         }
 
-        // Deactivate and delete LiteSpeed Cache plugin if it exists
-        if (is_plugin_active('litespeed-cache/litespeed-cache.php')) {
-            deactivate_plugins('litespeed-cache/litespeed-cache.php');
-            delete_plugins(['litespeed-cache/litespeed-cache.php']);
+        if (!$this->can_delete_plugins()) {
+            error_log('Sivustamo: Ei oikeuksia poistaa lisäosia.');
+            return;
         }
 
-        // Remove Perfmatters settings
+        $plugins_to_remove = [
+            'perfmatters/perfmatters.php',
+            'litespeed-cache/litespeed-cache.php'
+        ];
+
+        foreach ($plugins_to_remove as $plugin) {
+            if (is_plugin_active($plugin)) {
+                deactivate_plugins($plugin);
+
+                if (function_exists('delete_plugins') && function_exists('request_filesystem_credentials')) {
+                    $deleted = delete_plugins([$plugin]);
+                    if ($deleted === false) {
+                        error_log("Sivustamo: Lisäosan $plugin poisto epäonnistui.");
+                    } else {
+                        error_log("Sivustamo: Lisäosa $plugin poistettu onnistuneesti.");
+                    }
+                } else {
+                    error_log("Sivustamo: Lisäosan $plugin poisto ei ole mahdollista tällä hetkellä.");
+                }
+            }
+        }
+
+        // Poista asetukset
         delete_option('perfmatters_options');
 
-        // Remove LiteSpeed Cache settings
         global $wpdb;
         $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE 'litespeed.conf%'");
 
-        // Remove the shortcode functionality
         remove_shortcode('sivustamo_link');
+    }
+
+    public function add_custom_header()
+    {
+        if (is_admin() && !headers_sent()) {
+            header("X-Sivustamo-Plugin: Authorized");
+        }
     }
 }
 
 $si = new Sivustamo();
 
+// Aktivointi- ja deaktivointihookit
+register_activation_hook(__FILE__, array($si, 'activate'));
+register_deactivation_hook(__FILE__, array($si, 'deactivate'));
+
 /* 404 ohjaus etusivulle */
-if ( (!function_exists('redirect_404s') ) && (get_option('redirect_404s')==1) ) {
+if ((!function_exists('redirect_404s')) && (get_option('redirect_404s') == 1)) {
     function redirect_404s()
     {
         if (is_404()) {
@@ -189,7 +261,7 @@ if ( (!function_exists('redirect_404s') ) && (get_option('redirect_404s')==1) ) 
 }
 
 /* update sähköpostien esto */
-if ( (!function_exists('wpb_stop_update_emails') ) && (get_option('wpb_stop_update_emails')==1) ){
+if ((!function_exists('wpb_stop_update_emails')) && (get_option('wpb_stop_update_emails') == 1)) {
     function wpb_stop_update_emails($send, $type, $core_update, $result)
     {
         if (!empty($type) && $type == 'success') {
@@ -204,7 +276,7 @@ if ( (!function_exists('wpb_stop_update_emails') ) && (get_option('wpb_stop_upda
 }
 
 /* back to wordpress näppäin pois */
-if  (get_option('backtopwp')==1) {
+if (get_option('backtopwp') == 1) {
     add_action('admin_footer', function () {
         ?>
         <style>
@@ -242,7 +314,7 @@ if  (get_option('backtopwp')==1) {
 }
 
 /* ylläpitoteksti asiakkaalle */
-if ( (!function_exists('sivustamo_tukiviesti') ) && (get_option('sivustamo_tukiviesti')==1) ){
+if ((!function_exists('sivustamo_tukiviesti')) && (get_option('sivustamo_tukiviesti') == 1)) {
     add_action('wp_dashboard_setup', 'sivustamo_tukiviesti');
 
     function sivustamo_tukiviesti()
@@ -299,3 +371,5 @@ if ( (!function_exists('sivustamo_tukiviesti') ) && (get_option('sivustamo_tukiv
         ';
     }
 }
+
+?>
